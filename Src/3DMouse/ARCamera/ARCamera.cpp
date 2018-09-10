3f25ff116c84763795a925ef4c6d4ae132ac84d9
jjuiddong <jjuiddong@gmail.com>
2018-09-10 09:23:15 +0900

    update vs2017 solution

--- Src/3DMouse/ARCamera/ARCamera.cpp
+++ Src/3DMouse/ARCamera/ARCamera.cpp
@@ -4,8 +4,8 @@
 //
 
 #include "../../../../Common/Common/common.h"
-#include "../../../../Common/Graphic/graphic.h"
-#include "../../../../Common/Framework/framework.h"
+#include "../../../../Common/Graphic11/graphic11.h"
+#include "../../../../Common/Framework11/framework11.h"
 
 using namespace common;
 
